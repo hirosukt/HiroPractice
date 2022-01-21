e48101b6eeab2be4e61e9327a897f3ac7c46e015
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-01-22 03:46:41 +0900

    add: cancel break/place events

--- src/main/java/works/hirosuke/hiropractice/event/PlayerEvent.kt
+++ src/main/java/works/hirosuke/hiropractice/event/PlayerEvent.kt
@@ -1,8 +1,11 @@
 package works.hirosuke.hiropractice.event
 
+import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.player.PlayerInteractEvent
 
 class PlayerEvent: Listener {
@@ -10,5 +13,15 @@ class PlayerEvent: Listener {
     @EventHandler
     fun on(e: PlayerInteractEvent) {
         if (e.action == Action.PHYSICAL) e.isCancelled = true
+    }
+
+    @EventHandler
+    fun on(e: BlockBreakEvent) {
+        if (e.player.gameMode != GameMode.CREATIVE) e.isCancelled = true
+    }
+
+    @EventHandler
+    fun on(e: BlockPlaceEvent) {
+        if (e.player.gameMode != GameMode.CREATIVE) e.isCancelled = true
     }
 }
