f64688a727272b59dd053c4cf4dbade77e8d0f15
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-01-22 04:13:23 +0900

    add: enqueue/dequeue method

--- src/main/java/works/hirosuke/hiropractice/queue/QueueManager.kt
+++ src/main/java/works/hirosuke/hiropractice/queue/QueueManager.kt
@@ -1,5 +1,6 @@
 package works.hirosuke.hiropractice.queue
 
+import org.bukkit.entity.Player
 import works.hirosuke.hiropractice.match.EnumMatch
 
 object QueueManager {
@@ -11,5 +12,14 @@ object QueueManager {
     fun searchStartableQueue(): EnumMatch? {
         EnumMatch.values().forEach { if (QueueData.queues.filter { queue -> queue.type == it }.size > 1) return it }
         return null
+    }
+
+    fun enqueue(match: EnumMatch, player: Player) {
+        val queue = Queue(match, player)
+        if (queue !in QueueData.queues) QueueData.queues.add(queue)
+    }
+
+    fun dequeue(match: EnumMatch, player: Player) {
+        QueueData.queues.remove(Queue(match, player))
     }
 }
